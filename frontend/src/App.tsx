c6e0d82f80d39fe80ca1e285ba569fb56ddd2346
Abel <abel_borges@yahoo.com.br>
2022-11-16 21:42:50 -0300

    Resolvendo problemas de merge do git

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import NotificationButton from "./components/NotificationButton"
 
 function App() {
@@ -7,20 +6,7 @@ function App() {
     <>
       <h1>Olá Mundo!</h1>
       <NotificationButton />
-      <NotificationButton />
     </>
-=======
-function App() {
-
-  return (
-    <h1>Olá mundo!</h1>
->>>>>>> 5d4833b36dba12750fe34302fa272e62414f875e
-=======
-function App() {
-
-  return (
-    <h1>Olá mundo!</h1>
->>>>>>> 5d4833b36dba12750fe34302fa272e62414f875e
   )
 }
 
